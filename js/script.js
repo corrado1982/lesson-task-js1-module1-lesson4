2789db288d81c54d5a0484ddc1db331074d0479e
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-10-23 12:40:10 +0200

    s

--- js/script.js
+++ js/script.js
@@ -34,4 +34,35 @@ printNumbers(3, 6);
 
 //Question 3
 
-function addNumbers()
+function addNumbers(num1, num2, num3) {
+    let result = num1 + num2 + num3;
+    //console.log(result) ;
+    return result;
+}
+let result = addNumbers(3, 3, 3)
+let innerResult = document.querySelector(".total");
+innerResult.innerHTML = result;
+// console.log(result);
+
+
+//Question 4
+
+function createGreeting(someText) {
+    let newString = someText;
+    return newString;
+}
+
+let insertString = createGreeting("Hello, my name is");
+
+innerNewString = document.querySelector("#name");
+innerNewString.innerHTML = insertString;
+
+//Question 5
+
+function printListItems(listItems) {
+    for (let i = 0; i < listItems.length; i++){
+        console.log(listItems[i]);
+    }
+}
+
+printListItems(sports);
